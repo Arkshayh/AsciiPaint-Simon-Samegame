a93edb0e8f7a567c8ef14f48ab0a09130bad0e31
55019 <55019@etu.he2b.be>
2021-09-30 14:54:41 +0200

    adding javadoc and move method complete

--- src/asciipaint/Circle.java
+++ src/asciipaint/Circle.java
@@ -1,10 +1,22 @@
 package asciipaint;
 
+/**
+ * @author Cotton Ian, g55019
+ * a circle is a shape has a point (its center), a radius and a color
+ */
+
 public class Circle extends ColoredShape{
 
     private double radius;
     private Point center;
 
+    /**
+     * constructor of a circle
+     * @param center Point
+     * @param radius double
+     * @param color char
+     * @throws IllegalArgumentException if the radius is smaller or equals to 0
+     */
     public Circle (Point center , double radius, char color){
         super(color);
         if(radius <= 0){
@@ -14,17 +26,24 @@ public class Circle extends ColoredShape{
         this.center = new Point(center);
     }
 
+    /**
+     * changes the position of the circle, this is defined by its center attribute,
+     * this method will therefore move this point according to the coordinates given in parameter
+     * @param dx double
+     * @param dy double
+     */
     @Override
     public void move(double dx, double dy) {
-
+        this.center.move(dx, dy);
     }
 
+    /**
+     * check if the given point is inside the circle if not -> return false otherwise true
+     * @param p Point
+     * @return boolean
+     */
     @Override
     public boolean isInside(Point p) {
-        /**
-         * double distanceFromCenter = Math.sqrt( ((p.getX() - center.getX())*(p.getX() - center.getX())) +
-         *                 (p.getY() - center.getY())*(p.getY() - center.getY()));
-         */
         if(p.distanceTo(center) <= radius){
             return true;
         }
