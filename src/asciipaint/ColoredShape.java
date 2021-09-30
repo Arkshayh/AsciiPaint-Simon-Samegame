24caa48715422d7f046fae01a326f5416b38663b
55019 <55019@etu.he2b.be>
2021-09-30 14:29:37 +0200

    adding javadoc

--- src/asciipaint/ColoredShape.java
+++ src/asciipaint/ColoredShape.java
@@ -1,17 +1,36 @@
 package asciipaint;
 
+/**
+ * @author Cotton Ian g55019
+ * Abstract class that implements the shape interface.
+ * this class will be inherited by all others forms.
+ * This class represents the color of each shape and its methods regarding the color.
+ */
+
 public abstract class ColoredShape implements Shape{
     private char color;
 
+    /**
+     * constructoer
+     * @param color char
+     */
     public ColoredShape(char color) {
         this.color = color;
     }
 
+    /**
+     * getter
+     * @return char
+     */
     @Override
     public char getColor() {
         return color;
     }
 
+    /**
+     * never used but is in the the diagram
+     * @param color char
+     */
     public void setColor(char color) {
         this.color = color;
     }
