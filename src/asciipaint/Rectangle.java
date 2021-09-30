7db321f71988780d36d95e559a6f1ca9ffef263e
55019 <55019@etu.he2b.be>
2021-09-30 14:23:37 +0200

    adding javadoc

--- src/asciipaint/Rectangle.java
+++ src/asciipaint/Rectangle.java
@@ -1,11 +1,25 @@
 package asciipaint;
 
+/**
+ * @author Cotton Ian, g55019
+ * the rectangle class defines a rectangle using a point, its width, height and color.
+ * This class extends the ColorShaped abstract class
+ */
+
 public class Rectangle extends ColoredShape {
 
     private Point upperLeft;
     private double width;
     private double height;
 
+    /**
+     * Constructor of a rectangle
+     * @param upperLeft Point
+     * @param width double
+     * @param height double
+     * @param color char
+     * @throws IllegalArgumentException if the width/height <= 0
+     */
     public Rectangle(Point upperLeft, double width, double height, char color) {
         super(color);
         if(height <= 0 || width <= 0){
@@ -18,11 +32,23 @@ public class Rectangle extends ColoredShape {
         this.height = height;
     }
 
+    /**
+     * changes the position of the rectangle, this is defined by its upperleft attribute,
+     * this method will therefore move this point according to the coordinates given in parameter
+     * @param dx double
+     * @param dy double
+     */
     @Override
     public void move(double dx, double dy) {
         this.upperLeft.move(dx, dy);
     }
 
+    /**
+     * return a boolean : false if the point given in parameter is not consider inside the rectangle, $
+     * true if it's the case
+     * @param p Point
+     * @return boolean
+     */
     @Override
     public boolean isInside(Point p) {
         double xMin = this.upperLeft.getX();
