eb95dfe50c05ad275bce34542bdfd7a1ee320c28
55019 <55019@etu.he2b.be>
2021-09-30 14:16:00 +0200

    adding tests for the rectangle class

---
+++ src/test/RectangleTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import asciipaint.Point;
+import asciipaint.Rectangle;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * @author Ian Cotton | g55019
+ */
+
+
+public class RectangleTest {
+    private Rectangle rectangle;
+
+    @BeforeEach
+    public void setUp(){
+        rectangle = new Rectangle(new Point(10,10), 10,5,'R');
+    }
+
+    @Test
+    public void width_small_or_equals_zero(){
+       assertThrows(IllegalArgumentException.class,
+               () -> new Rectangle(new Point(10,10),0,10,'R'));
+    }
+
+    @Test
+    public void height_small_or_equals_zero(){
+        assertThrows(IllegalArgumentException.class,
+                () -> new Rectangle(new Point(10,10),0,10,'R'));
+    }
+
+    @Test
+    public void is_inside_true(){
+        assertTrue(rectangle.isInside(new Point(10,10)));
+    }
+
+    @Test
+    public void is_inside_false(){
+        assertFalse(rectangle.isInside(new Point(5,5)));
+    }
+}
