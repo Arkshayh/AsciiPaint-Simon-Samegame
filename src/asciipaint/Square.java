6af45d443317952e9b8b65678f6462f5bb2efd2e
55019 <55019@etu.he2b.be>
2021-09-30 14:16:11 +0200

    adding javadoc

--- src/asciipaint/Square.java
+++ src/asciipaint/Square.java
@@ -1,6 +1,21 @@
 package asciipaint;
 
+/**
+ * @author g55019 / Cotton Ian
+ * This square class extends the Rectangle class.
+ * A square is a rectangle with the only difference that all its sides are identical.
+ * A square must have a Point, the size of one side (int) and a color (char) to be created.
+ */
+
 public class Square extends Rectangle{
+
+    /**
+     * Constructor of a Square, The side of a square must be >= 0.
+     * @param upperLeft
+     * @param side
+     * @param color
+     */
+
     public Square(Point upperLeft, double side, char color) {
         super(upperLeft, side, side, color);
     }
