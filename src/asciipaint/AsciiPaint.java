631e97f9add44ffd3ce08254895eff3dcf4e498d
55019 <55019@etu.he2b.be>
2021-10-01 15:23:28 +0200

    adding javadoc

--- src/asciipaint/AsciiPaint.java
+++ src/asciipaint/AsciiPaint.java
@@ -2,27 +2,67 @@ package asciipaint;
 
 public class AsciiPaint {
 
+    /**
+     * @author Cotton Ian g55019
+     * This class has the methods allowing to add shapes to the drawings
+     * as well as to return the drawing as a string
+     */
+
     private Drawing drawing;
 
+    /**
+     * Constructor when you choose the dimension of the drawing
+     * @param drawing
+     */
     public AsciiPaint(Drawing drawing) {
         this.drawing = drawing;
     }
 
+    /**
+     * default constructor, the dimension of the drawing will be 50x50
+     */
     public AsciiPaint() {
+        drawing = new Drawing();
     }
 
+    /**
+     * add a circle to the drawing
+     * @param x int
+     * @param y int
+     * @param radius double
+     * @param color char
+     */
     public void newCircle(int x, int y, double radius, char color){
         drawing.addShape(new Circle(new Point(x,y),radius,color));
     }
 
+    /**
+     * add a rectangle to the drawing
+     * @param x int
+     * @param y int
+     * @param width double
+     * @param height double
+     * @param color char
+     */
     public void newRectangle(int x, int y, double width, double height, char color){
         drawing.addShape(new Rectangle(new Point(x,y), width, height, color));
     }
 
+    /**
+     * add a square to the drawing
+     * @param x int
+     * @param y int
+     * @param side double
+     * @param color char
+     */
     public void newSquare(int x, int y, double side, char color){
         drawing.addShape(new Square(new Point(x,y),side,color));
     }
 
+    /**
+     * Return the drawing
+     * @return String as a string.
+     */
     public String asAscii(){
         String monstring = "";
 
