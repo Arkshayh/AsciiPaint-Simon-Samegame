bc7d42f6820cf379e222f457aec71327e2a7415f
55019 <55019@etu.he2b.be>
2021-10-15 14:07:37 +0200

    creation of the model class who implement observable. Override the method

---
+++ modules/simon/src/main/java/esi/g55019/atl/simon/Model/Model.java
@@ -0,0 +1,26 @@
+package esi.g55019.atl.simon.Model;
+
+import esi.g55019.atl.simon.util.Observable;
+import esi.g55019.atl.simon.util.Observer;
+import java.util.List;
+
+public class Model implements Observable {
+    private List<Observer> listObserver;
+
+    @Override
+    public void addObserver(Observer observer) {
+        listObserver.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        listObserver.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (int i = 0; i < listObserver.size(); i++) {
+            listObserver.get(i).update();
+        }
+    }
+}
