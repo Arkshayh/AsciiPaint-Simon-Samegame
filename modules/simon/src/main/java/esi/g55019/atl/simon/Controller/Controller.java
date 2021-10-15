0263e4c72457a2c7bd85dcc28d5349c089ae16fa
55019 <55019@etu.he2b.be>
2021-10-15 14:32:41 +0200

    adding attributes and changing constructor

--- modules/simon/src/main/java/esi/g55019/atl/simon/Controller/Controller.java
+++ modules/simon/src/main/java/esi/g55019/atl/simon/Controller/Controller.java
@@ -1,18 +1,21 @@
 package esi.g55019.atl.simon.Controller;
 
+import esi.g55019.atl.simon.Model.Model;
 import esi.g55019.atl.simon.View.View;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Controller extends Application {
+    private Model model;
     private View view;
 
-    public Controller() {
+    public Controller(Model model) {
+        this.model = model;
+        this.view = new View(this, model);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        this.view = new View(primaryStage);
-        view.start();
+        view.start(primaryStage);
     }
 }
