dd3beebe27516b1a193c45fbef23d5ab1b42afba
55019 <55019@etu.he2b.be>
2021-10-16 16:42:42 +0200

    adding method when button pushed

--- modules/simon/src/main/java/esi/g55019/atl/simon/Controller/Controller.java
+++ modules/simon/src/main/java/esi/g55019/atl/simon/Controller/Controller.java
@@ -1,6 +1,8 @@
 package esi.g55019.atl.simon.Controller;
 
+import esi.g55019.atl.simon.Model.Color;
 import esi.g55019.atl.simon.Model.Model;
+import esi.g55019.atl.simon.Model.State;
 import esi.g55019.atl.simon.View.View;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -14,8 +16,52 @@ public class Controller extends Application {
         this.view = new View(this, model);
     }
 
+    public void startOnClick(){
+        model.setState(State.AFFICHAGE_START);
+        model.addToStartList();
+        playDisplayStartSequence();
+    }
+
+    public void colorButtonOnClick(Color color){
+        addOnListeActuelle(color);
+        fullCheck();
+    }
+
+    private void fullCheck(){
+        //Si bon bouton choisi
+        if(model.checkCorrectColor()){
+            if(model.aRattraper()){
+                model.clearListPlayer();
+                model.setState(State.AFFICHAGE_START);
+                model.addToStartList();
+                playDisplayStartSequence();
+            }
+        }
+        else{
+            System.out.println("PERDU");
+            model.setState(State.ON_THE_MENU);
+            System.out.println("Controller fullcheck, UPDATE DE LASTLIST");
+            model.updateLastList();
+            if(model.getListStartColor().size() > model.getListLonguest().size()){
+                System.out.println("Controller fullcheck, UPDATE DE LONGUESTLIST");
+                model.setListLonguest();
+            }
+            model.clearStartListAndPlayerList();
+        }
+    }
+
+    private void playDisplayStartSequence(){
+        view.playSequenceColor(model.getListStartColor());
+    }
+
+    public void addOnListeActuelle(Color color){
+        model.addListeActuelle(color);
+    }
+
+
     @Override
     public void start(Stage primaryStage) {
         view.start(primaryStage);
+        model.notifyObservers();
     }
 }
