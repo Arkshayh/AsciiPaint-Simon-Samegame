0ab901bffbc72f7da714ce4100aa04d2a3304bee
55019 <55019@etu.he2b.be>
2021-10-17 20:14:34 +0200

    remove 2 useless states

--- modules/simon/src/main/java/esi/g55019/atl/simon/Model/State.java
+++ modules/simon/src/main/java/esi/g55019/atl/simon/Model/State.java
@@ -3,7 +3,5 @@ package esi.g55019.atl.simon.Model;
 public enum State {
     ON_THE_MENU,
     AFFICHAGE_START,
-    AFFICHAGE_LAST,
-    AFFICHAGE_LONGUEST,
-    PLAYER_CHOOSE;
+    PLAYER_CHOOSE
 }
