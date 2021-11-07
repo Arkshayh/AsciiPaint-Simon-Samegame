ef0f5886aeaeaf21a5dcebc92f07163bc5323d0f
55019 <55019@etu.he2b.be>
2021-11-07 18:32:42 +0100

    adding javadoc

--- modules/asciipaint/src/main/java/esi/g55019/atl/asciipaint/AsciiPaint.java
+++ modules/asciipaint/src/main/java/esi/g55019/atl/asciipaint/AsciiPaint.java
@@ -70,10 +70,25 @@ public class AsciiPaint {
         drawing.addShape(new Leaf(new Square(new Point(x, y), side, color)));
     }
 
+    /**
+     * add a line to the drawing
+     * @param x1    int
+     * @param y1    int
+     * @param x2    int
+     * @param y2    int
+     * @param color char
+     */
     public void newLine(int x1, int y1, int x2, int y2, char color){
         drawing.addShape(new Leaf(new Line(new Point(x1,y1), new Point(x2,y2), color)));
     }
 
+    /**
+     * create a group to the drawing, the param has the index of each Leaf/group to add in the group
+     * First each Component will be added in the created group
+     * Second each Compenent will be delete from the drawing
+     * Third the group will be added to the drawing
+     * @param commandes int[]
+     */
     public void newGroup(int[] commandes){
         Composite groupe = new Composite();
         for (int i = 0; i < commandes.length; i++) {
@@ -91,10 +106,18 @@ public class AsciiPaint {
         drawing.addShape(groupe);
     }
 
+    /**
+     * add a Leaf or a Composite to the drawing
+     * @param composant
+     */
     public void addCompenent(Component composant){
         drawing.addShape(composant);
     }
 
+    /**
+     * remove a Component from the drawing
+     * @param compenent Component
+     */
     public void removeShape(Component compenent){
         drawing.removeShape(compenent);
     }
@@ -105,7 +128,6 @@ public class AsciiPaint {
      */
     public String asAscii() {
         String monstring = "";
-        //TODO: question prof si optimisation ok + explication
         for (int i = drawing.getHeight(); i >= 0; i--) {
             for (int j = 0; j < drawing.getWidth(); j++) {
                 if (drawing.getShapeAt(new Point(j, i)) != null) {
@@ -120,30 +142,60 @@ public class AsciiPaint {
         return monstring;
     }
 
+    /**
+     * called the showlist method
+     */
     public void showList(){
         drawing.showList();
     }
 
+    /**
+     * called the move method
+     * @param numberOfTheShape  int
+     * @param dx    int
+     * @param dy    int
+     */
     public void move(int numberOfTheShape, int dx, int dy){
         drawing.move(numberOfTheShape, dx, dy);
     }
 
+    /**
+     * return the number of component from the drawing
+     * @return
+     */
     public int nbForme(){
         return drawing.getSize();
     }
 
+    /**
+     * set the attribut end of asciipaint to end
+     */
     public void end(){
         this.end = true;
     }
 
+    /**
+     * return the shape at the given index
+     * @param index int
+     * @return Component
+     */
     public Component getShapeAt(int index){
         return drawing.getShapeInList(index);
     }
 
+    /**
+     * change the color of the Component at the given position
+     * @param index int
+     * @param color color
+     */
     public void changeColor(int index, char color){
         drawing.changeColor(index, color);
     }
 
+    /**
+     * return the value of the end attribute
+     * @return boolean
+     */
     public boolean getEnd(){
         return end;
     }
