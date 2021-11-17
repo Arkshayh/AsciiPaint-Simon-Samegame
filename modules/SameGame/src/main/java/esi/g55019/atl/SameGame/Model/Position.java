784b418e2eee8f2a50eef94d28b7d49cae957d47
55019 <55019@etu.he2b.be>
2021-11-17 17:37:19 +0100

    adding javadoc

--- modules/SameGame/src/main/java/esi/g55019/atl/SameGame/Model/Position.java
+++ modules/SameGame/src/main/java/esi/g55019/atl/SameGame/Model/Position.java
@@ -1,28 +1,37 @@
 package esi.g55019.atl.SameGame.Model;
 
+/**
+ * this class represent a position.
+ * A position has the number of his line, the number of his column
+ */
 public class Position {
     private int Ligne;
     private int Colonne;
 
+    /**
+     * constructor
+     * @param ligne int
+     * @param colonne int
+     */
     public Position(int ligne, int colonne) {
         this.Ligne = ligne;
         this.Colonne = colonne;
     }
 
+    /**
+     * getter for colonne
+     * @return int
+     */
     public int getColonne() {
         return Colonne;
     }
 
-    public void setColonne(int colonne) {
-        this.Colonne = colonne;
-    }
-
+    /**
+     * getter for ligne
+     * @return int
+     */
     public int getLigne() {
         return Ligne;
-    }
-
-    public void setLigne(int ligne) {
-        this.Ligne = ligne;
     }
 
     @Override
