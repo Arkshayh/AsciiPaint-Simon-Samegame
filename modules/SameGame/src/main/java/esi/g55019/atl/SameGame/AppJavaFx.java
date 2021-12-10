12187ed1f0bef438f97ff30fc1e2bc8193a0733b
55019 <55019@etu.he2b.be>
2021-12-10 19:56:47 +0100

    test

--- modules/SameGame/src/main/java/esi/g55019/atl/SameGame/AppJavaFx.java
+++ modules/SameGame/src/main/java/esi/g55019/atl/SameGame/AppJavaFx.java
@@ -1,20 +1,17 @@
 package esi.g55019.atl.SameGame;
 
-
 import esi.g55019.atl.SameGame.ControllerJavaFx.ControllerJavaFx;
 import esi.g55019.atl.SameGame.Model.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+
 
 public class AppJavaFx extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
-    /**
-     * 9) Ajouter popup win en vert avec musique victoire si gagné sinon lose pop up perdu lose avec rire veigar
-     * 10) AJOUTER SCORE | AJOUTER RESTART
-     */
     @Override
     public void start(Stage primaryStage){
         Model model  = new Model();
