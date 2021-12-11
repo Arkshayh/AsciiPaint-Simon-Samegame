f10fe14b0cc9697c7346a5b3277aef4605a1d01d
55019 <55019@etu.he2b.be>
2021-12-11 19:04:10 +0100

    adding javadoc

--- modules/SameGame/src/main/java/esi/g55019/atl/SameGame/DPCommand/ClickBilleCommand.java
+++ modules/SameGame/src/main/java/esi/g55019/atl/SameGame/DPCommand/ClickBilleCommand.java
@@ -4,16 +4,29 @@ import esi.g55019.atl.SameGame.Model.Board;
 import esi.g55019.atl.SameGame.Model.Model;
 import esi.g55019.atl.SameGame.Model.Position;
 
+/**
+ * This class implement Command. And represent a Command when the user want to remove a Bille.
+ */
 public class ClickBilleCommand implements Command{
     private Model model;
     private Board oldBoard;
     private Position position;
 
+    /**
+     * Constructor
+     * @param model Model
+     * @param position Position
+     */
     public ClickBilleCommand(Model model, Position position) {
         this.model = model;
         this.position = position;
     }
 
+    /**
+     * return true if the command can correctly be executed.
+     * it will call the method play of the model.
+     * @return boolean
+     */
     @Override
     public boolean execute() {
         this.oldBoard = model.getBoard();
@@ -23,11 +36,18 @@ public class ClickBilleCommand implements Command{
         return false;
     }
 
+    /**
+     * Will change the board of the model with the oldBoard of the model
+     */
     @Override
     public void unexecute() {
         this.model.setBoard(oldBoard);
     }
 
+    /**
+     * return true if this command has an unexecute method
+     * @return boolean
+     */
     @Override
     public boolean isReversible() {
         return true;
