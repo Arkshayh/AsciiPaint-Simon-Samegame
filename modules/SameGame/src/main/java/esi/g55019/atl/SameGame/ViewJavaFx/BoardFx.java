eac66501eac6cca11de832df9e1cfb3a1244f067
55019 <55019@etu.he2b.be>
2021-12-11 19:27:28 +0100

    adding javadoc

--- modules/SameGame/src/main/java/esi/g55019/atl/SameGame/ViewJavaFx/BoardFx.java
+++ modules/SameGame/src/main/java/esi/g55019/atl/SameGame/ViewJavaFx/BoardFx.java
@@ -9,22 +9,37 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
+/**
+ * This class represent the board to be show for the javaFx app
+ */
 public class BoardFx {
     private Button[][] buttonBoard;
     private GridPane boardPane;
     private Board board;
     private ControllerJavaFx controllerJavaFx;
 
+    /**
+     * Constructor
+     * @param board Board
+     * @param controllerJavaFx ControllerJavaFx
+     */
     public BoardFx(Board board, ControllerJavaFx controllerJavaFx) {
         this.board = board;
         this.controllerJavaFx = controllerJavaFx;
         this.boardPane = new GridPane();
-        this.buttonBoard = new Button[board.getLigne()][board.getColonne()];
+        this.buttonBoard = new Button[board.getRow()][board.getColumn()];
         updateBoard();
     }
 
+    /**
+     * Update the button of the board
+     * It will check the board attribute. If the object at the position of the board is not null then the button of the
+     * BoardJavaFx will have a setOnAction method an a color (not black)
+     *
+     * If null then the button will be black and will not have the setOnAction method
+     */
     public void updateBoard() {
-        buttonBoard = new Button[board.getLigne()][board.getColonne()];
+        buttonBoard = new Button[board.getRow()][board.getColumn()];
         Bille[][] plateauDeBille = board.getPlateau();
         for (int i = 0; i < plateauDeBille.length; i++) {
             for (int j = 0; j < plateauDeBille[0].length; j++) {
@@ -59,11 +74,12 @@ public class BoardFx {
         }
     }
 
+    /**
+     * Getter for the boardPane
+     * @return GridPane
+     */
     public GridPane getBoardPane() {
         return boardPane;
     }
 
-    public void setBoard(Board board){
-        this.board = board;
-    }
 }
